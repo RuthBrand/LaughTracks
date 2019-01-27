0df5bba1cb61d91eaedade43cad36a4ed9e90608
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-01-27 13:31:26 -0700

    wip

---
+++ spec/features/comedians_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+RSpec.describe do
+  describe "seeing basic comedian information on /comedians" do
+       it "displays comedians baseic information" do 
+       visit '/comedians'
+
+       @comedian = Comedian.create("George Carlin", 72, "Houston")
+
+       expect(page).to have_content(@comedian.name)
+       expect(page).to have_content(@comedian.age)
+       expect(page).to have_content(@comedian.city)
+    end
+  end
+end
